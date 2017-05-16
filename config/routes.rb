d7069b7fe63fa53c97a7b61a7fde31ef3b0574f6
robeihmanis <reihmanis@gmail.com>
2017-05-16 19:46:08 +0300

    Add styles, add other pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
+  resources :welcome do
+    collection do
+      get 'about'
+      get 'contact'
+    end
 
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
